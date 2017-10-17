72d4590aa76108c048d02437fd6e459aa8f0aa6a
Shoaib Kamil <kamil@adobe.com>
2017-10-17 12:57:59 -0400

    More progress; debugging

--- cg/cg.cpp
+++ cg/cg.cpp
@@ -20,7 +20,7 @@
     iter = iter + 1;
   end
 **/
-
+#include <cstdio>
 #include "Halide.h"
 
 #include "saxpy.h"
@@ -32,14 +32,14 @@
 using namespace Halide;
 
 // wrap a vector in a halide_buffer_t
-void wrap_vector(halide_buffer_t* ret, float* data, int size) {
+inline void wrap_vector(halide_buffer_t* ret, float* data, int size) {
 
   ret->device = 0;
   ret->device_interface = 0;
   ret->host = (uint8_t*)data;
   ret->flags = 0;
   ret->type = halide_type_of<float>();
-  ret->dimensions = 2;
+  ret->dimensions = 1;
 
   ret->dim = (halide_dimension_t*)malloc(sizeof(halide_dimension_t));
   ret->dim[0].min = 0;
@@ -48,6 +48,64 @@ void wrap_vector(halide_buffer_t* ret, float* data, int size) {
 
   ret->set_device_dirty();
 }
+
+// wrap a matrix in a halide_buffer_t
+inline void wrap_matrix(halide_buffer_t* ret, float* data, int size) {
+
+  ret->device = 0;
+  ret->device_interface = 0;
+  ret->host = (uint8_t*)data;
+  ret->flags = 0;
+  ret->type = halide_type_of<float>();
+  ret->dimensions = 2;
+
+  ret->dim = (halide_dimension_t*)malloc(2*sizeof(halide_dimension_t));
+  ret->dim[0].min = 0;
+  ret->dim[0].extent = size;
+  ret->dim[0].stride = 1;
+
+  ret->dim[1].min = 0;
+  ret->dim[1].extent = size;
+  ret->dim[1].stride = size;
+
+
+  ret->set_device_dirty();
+}
+
+void print_vec(std::string name, float* vec, int size) {
+  printf("%s: ", name.c_str());
+  for (int i=0; i<size; i++)
+    printf("%3.3f ", vec[i]);
+  printf("\n");
+}
+
+void print_vec(std::string name, halide_buffer_t* vec, int size) {
+  print_vec(name, (float*)(vec->host), size);
+}
+
+void generate_test_data(float** mat, float** b) {
+  float *mat_ret = (float*)malloc(sizeof(float) * 9);
+  float *b_ret = (float*)malloc(sizeof(float) * 3);
+
+  mat_ret[0] = 5;
+  mat_ret[1] = 1;
+  mat_ret[2] = 0.5;
+  mat_ret[3] = 0.4;
+  mat_ret[4] = 5;
+  mat_ret[5] = 0.3;
+  mat_ret[6] = 0.6;
+  mat_ret[7] = 0.8;
+  mat_ret[8] = 5;
+
+  b_ret[0] = 1.f;
+  b_ret[1] = 2.f;
+  b_ret[2] = 3.f;
+
+  *mat = mat_ret;
+  *b = b_ret;
+
+  // expected result: {0.074, 0.362, 0.533}
+} 
 
 // CG
 // Inputs: Matrix A, RHS vector b, tolerance, maxiters
@@ -73,6 +131,7 @@ void cg_1(halide_buffer_t* A, halide_buffer_t* b, float tol, int maxiters,
 
   // calculate residual
   sgemv(1.0f, A, x, 0.0f, tmp, tmp);
+  print_vec("tmp", tmp, 3);
   svecsub(tmp, b, r);
 
   // calculate norm squared
@@ -112,6 +171,37 @@ int main(int argc, char* argv[]) {
   float tol = 1e-4;
   int maxiters = 100;
 
+  // generate test data
+  float* mat;
+  float* b;
+  halide_buffer_t mat_buf;
+  halide_buffer_t b_buf;
 
+  generate_test_data(&mat, &b);
+  wrap_vector(&b_buf, b, 3);
+  wrap_matrix(&mat_buf, mat, 3);
 
+  // create x and temporaries
+  float x[3] = {0};
+  float p[3] = {0};
+  float r[3] = {0};
+  float tmp[3] = {0};
+  halide_buffer_t x_buf;
+  halide_buffer_t p_buf;
+  halide_buffer_t r_buf;
+  halide_buffer_t tmp_buf;
+
+  wrap_vector(&x_buf, x, 3);
+  wrap_vector(&p_buf, p, 3);
+  wrap_vector(&r_buf, r, 3);
+  wrap_vector(&tmp_buf, tmp, 3);
+
+  // call CG
+  cg_1(&mat_buf, &b_buf, tol, maxiters,
+       &x_buf,
+       &p_buf, &r_buf, &tmp_buf);
+
+  print_vec("x", x, 3);
+
+  return 0;
 }
