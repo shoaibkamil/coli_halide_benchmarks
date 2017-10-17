7cbde2d7ad036b7bdd394d6de3a009d73ee11e3a
Shoaib Kamil <kamil@adobe.com>
2017-10-17 12:21:08 -0400

    Compiles

--- cg/cg.cpp
+++ cg/cg.cpp
@@ -23,22 +23,60 @@
 
 #include "Halide.h"
 
+#include "saxpy.h"
+#include "sdot.h"
+#include "sgemv.h"
+#include "sveccopy.h"
+#include "svecsub.h"
+
 using namespace Halide;
+
+// wrap a vector in a halide_buffer_t
+void wrap_vector(halide_buffer_t* ret, float* data, int size) {
+
+  ret->device = 0;
+  ret->device_interface = 0;
+  ret->host = (uint8_t*)data;
+  ret->flags = 0;
+  ret->type = halide_type_of<float>();
+  ret->dimensions = 2;
+
+  ret->dim = (halide_dimension_t*)malloc(sizeof(halide_dimension_t));
+  ret->dim[0].min = 0;
+  ret->dim[0].extent = size;
+  ret->dim[0].stride = 1;
+
+  ret->set_device_dirty();
+}
 
 // CG
 // Inputs: Matrix A, RHS vector b, tolerance, maxiters
+// Temporaries: p, r
 // Outputs: x, where A*x = b
 void cg_1(halide_buffer_t* A, halide_buffer_t* b, float tol, int maxiters,
-          halide_buffer_t* x) {
+          halide_buffer_t* x,
+          halide_buffer_t* p, halide_buffer_t* r, halide_buffer_t* tmp) {
   // allocate temporaries
   int iter = 0;
+  float normr2;
+  float alpha;
+  float normr2old;
+  float denom;
+  float beta;
+
+  halide_buffer_t normr2_buf;
+  halide_buffer_t denom_buf;
+
+  // wrap 0-dim temporaries
+  wrap_vector(&normr2_buf, &normr2, 1);
+  wrap_vector(&denom_buf, &denom, 1);
 
   // calculate residual
-  sgemv(A, x, tmp);
+  sgemv(1.0f, A, x, 0.0f, tmp, tmp);
   svecsub(tmp, b, r);
 
   // calculate norm squared
-  sdot(r, r, normr2);
+  sdot(r, r, &normr2_buf);
 
   // copy r to p
   sveccopy(r, p);
@@ -46,27 +84,26 @@ void cg_1(halide_buffer_t* A, halide_buffer_t* b, float tol, int maxiters,
   // main loop
   while (normr2 > tol && iter < maxiters) {
     // calculate denom
-    sgemv(A, p, tmp);
-    sdot(r, tmp, denom);
+    sgemv(1.0f, A, p, 0.0f, tmp, tmp);
+    sdot(r, tmp, &denom_buf);
 
     alpha = normr2 / denom;
 
     // update x
     // TODO: make sure the order here is correct
-    saxpy(x, p, alpha, x);
+    saxpy(alpha, p, x, x);
 
     // update normr2
-    float normr2old = normr2;
-    saxpy(r, tmp, -alpha, r);
-    sdot(r, r, normr2);
+    normr2old = normr2;
+    saxpy(-alpha, tmp, r, r);
+    sdot(r, r, &normr2_buf);
 
     // update p
-    float beta = normr2 / normr2old;
-    saxpy(p, r, beta, p);
+    beta = normr2 / normr2old;
+    saxpy(beta, r, p, p);
     iter++;
   }
 }
-
 
 
 
