f070948c3ecfaa1095ad67e3149a0a7b7236a90a
Shoaib Kamil <kamil@adobe.com>
2018-03-19 11:37:23 -0400

    Use 4D RDom for heat3d

--- heat3d.cpp
+++ heat3d.cpp
@@ -12,22 +12,23 @@ int main(int argc, char* argv[]) {
   Var x, y, z;
   Var k;
 
-  RDom time{1, t, "t"};
+  RDom st{1, in.dim(0).extent()-1, 1, in.dim(1).extent()-1, 1, in.dim(2).extent()-1, 1, t, "spacetime"};
 
   heat3d(x, y, z, k) = undef<float>();
-  heat3d(x, y, z, 0) = alpha * in(x, y, z) +
-                 beta * (in(x+1, y, z) + in(x-1, y, z)+
-                         in(x, y+1, z) + in(x, y-1, z) +
-                         in(x, y, z+1) + in(x, y, z-1));
+  heat3d(st.x, st.y, st.z, 0) = alpha * in(st.x, st.y, st.z) +
+                 beta * (in(st.x+1, st.y, st.z) + in(st.x-1, st.y, st.z)+
+                         in(st.x, st.y+1, st.z) + in(st.x, st.y-1, st.z) +
+                         in(st.x, st.y, st.z+1) + in(st.x, st.y, st.z-1));
 
 
-  heat3d(x, y, z, time.x) = alpha * heat3d(x, y, z, time.x-1) +
-                 beta * (heat3d(x+1, y, z, time.x-1) + heat3d(x-1, y, z, time.x-1)+
-                         heat3d(x, y+1, z, time.x-1) + heat3d(x, y-1, z, time.x-1) +
-                         heat3d(x, y, z+1, time.x-1) + heat3d(x, y, z-1, time.x-1));
+  heat3d(st.x, st.y, st.z, (st.w+1)%2) = alpha * heat3d(st.x, st.y, st.z, st.w%2) +
+                  beta * (heat3d(st.x+1, st.y, st.z, st.w%2) + heat3d(st.x-1, st.y, st.z, st.w%2) +
+                          heat3d(st.x, st.y+1, st.z, st.w%2) + heat3d(st.x, st.y-1, st.z, st.w%2) +
+                          heat3d(st.x, st.y, st.z+1, st.w%2) + heat3d(st.x, st.y, st.z-1, st.w%2));
 
 
-  heat3d.parallel(z).vectorize(x, 8);
+  //heat3d.parallel(z).vectorize(x, 8);
+  heat3d.update(1).allow_race_conditions().parallel(st.z).vectorize(st.x, 8);
 
   heat3d.compile_to_lowered_stmt("heat3d.html", {in, alpha, beta, t}, HTML);
 
